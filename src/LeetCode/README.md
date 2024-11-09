00602ed91c84bee2d29bd2c8acbc620fabb26b3c
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2024-11-09 09:46:34 +0530

    Leetcode Solved Problems

---
+++ src/LeetCode/README.md
@@ -0,0 +1,2 @@
+# Leetcode Problems Solved
+1) Remove Duplicates From Array inplace. (q.no. 26 LC)
--- src/LeetCode/RemoveDuplicates.java
+++ src/LeetCode/RemoveDuplicates.java
@@ -41,7 +41,7 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int arr[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
         //IP: {0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5}
         System.out.println(Arrays.toString(arr));
         //OP:[0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
