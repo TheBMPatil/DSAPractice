b77565a110e20da3675866e9fdefc2b428125b8e
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2024-11-25 10:28:03 +0530

    Revesre LinkedList recursive & inplace

--- src/LinkedListsLearning/SLL.java
+++ src/LinkedListsLearning/SLL.java
@@ -229,6 +229,42 @@ public class SLL {
     }
 
 
+    //REVERSE LINKEDLIST
+
+    //Recursie
+    private void reverse(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+
+    }
+
+    //In-Place
+    private void reverse() {
+        if (size < 2) {
+            return;
+        }
+        Node prev = null;
+        Node pres = head;
+        Node next = pres.next;
+
+        while (pres != null) {
+            pres.next = prev;
+            prev = pres;
+            pres = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        head = prev;
+//        return head;
+    }
+
     public static void main(String[] args) {
         SLL first = new SLL();
         SLL second = new SLL();
